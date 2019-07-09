7c72c9af3e4d28281d44fe076ed88d11a62fc56a
julienbourgoin <julien.bourgoin@gmail.com>
2019-07-09 23:41:43 +0200

    formatting

--- izanami-webhook.sh
+++ izanami-webhook.sh
@@ -1,33 +1,33 @@
 #!/bin/bash
 
 function post_to_slack () {
-  # format message as a code block ```${msg}```
-  SLACK_MESSAGE="\`\`\`$1\`\`\`"
-  SLACK_URL=https://hooks.slack.com/services/$SLACK_SERVICE_IDENTIFIER
-  SLACK_ICON=':izanami:'
-  curl -s -X POST --data "payload={\"username\": \"Izanami\", \"icon_emoji\": \"${SLACK_ICON}\", \"text\": \"${SLACK_MESSAGE}\"}" ${SLACK_URL}
+    # format message as a code block ```${msg}```
+    SLACK_MESSAGE="\`\`\`$1\`\`\`"
+    SLACK_URL=https://hooks.slack.com/services/$SLACK_SERVICE_IDENTIFIER
+    SLACK_ICON=':izanami:'
+    curl -s -X POST --data "payload={\"username\": \"Izanami\", \"icon_emoji\": \"${SLACK_ICON}\", \"text\": \"${SLACK_MESSAGE}\"}" ${SLACK_URL}
 }
 
 _processFeatureObject()
 {
     object=$(echo $1  | base64 --decode)
-	type=$(echo $object  | jq -r '.type')
-	key=$(echo $object | jq -r '.key')
+    type=$(echo $object  | jq -r '.type')
+    key=$(echo $object | jq -r '.key')
     enabled=$(echo $object | jq -r '.payload.enabled')
     post_to_slack "$type : $key $enabled"
 }
 
 _processConfigObject()
 {
-	object=$(echo $1  | base64 --decode)
+    object=$(echo $1 | base64 --decode)
     echo 'Config object'
 }
 
 _processEditedObject()
 {
-	object=$(echo $1  | base64 --decode)
-	domain=$(echo $object | jq -r '.domain')
-	echo $domain
+    object=$(echo $1  | base64 --decode)
+    domain=$(echo $object | jq -r '.domain')
+    echo $domain
 
     case "$domain" in
         "Feature")
@@ -37,7 +37,7 @@ _processEditedObject()
         "Config")
             _processConfigObject $1
             ;;
-		*)
+        *)
             echo "Objet non reconnu : $domain"
     esac
 }
